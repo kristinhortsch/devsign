ddda1838478bcc54f9d58dc9d573f75844a2a138
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-13 09:39:36 -0700

    somewhat okay right now, in the middle of the mini prof styles

--- src/components/users/MiniProfile.js
+++ src/components/users/MiniProfile.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Aside = styled.aside`
+  border: 3px solid blue;
+  .background {
+    height: 70px;
+    z-index: -1;
+    width: 100%;
+  }
+  .profpic {
+    width: 50px;
+    margin-top: -35px;
+  }
+  h3 {
+    margin: 0px;
+  }
+  p {
+    margin: 0px;
+  }
+`;
 export default function MiniProfile() {
   return (
-    <aside>
-      <img src="../assets/prof-pic.png" width="30px" alt="prof pic"/>
-      <h3>User Name</h3>
+    <Aside>
+      <img src="../assets/background.jpg" alt="background" className="background"/>
+      <img src="../assets/prof-pic.png" alt="prof pic" className="profpic"/>
+      <div>
+        <h3>Kristin Hortsch</h3>
+        <p>@kristinhortsch</p>
+      </div>
       <hr />
-      <p>Chirps: 593</p>
-      <p>Followers: 388</p>
-    </aside>
+      <table>
+        <thead>
+          <tr>Chirps</tr>
+          <tr>Followers</tr>
+        </thead>
+        <tbody>
+          <td>593</td>
+          <td>388</td>
+        </tbody>
+      </table>
+      
+    </Aside>
   );
 }
