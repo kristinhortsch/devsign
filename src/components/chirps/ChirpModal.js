bd3c2d1e518e8633ed40c81190b9442d20cf552a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-13 17:27:21 -0700

    fixed new chirp modal

--- src/components/chirps/ChirpModal.js
+++ src/components/chirps/ChirpModal.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Section = styled.section`
+
 .backdropStyle {
   position: fixed;
   top: 0;
@@ -12,15 +13,15 @@ const Section = styled.section`
   width: 100vw;
   height: 100vh;
   z-index: 1;
-  background-color: rgba(178, 178, 187, 0.479);
+  background-color: rgba(178, 178, 187, 0.7);
   .chirpModal {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     background: white;
-    width: 60%;
-    margin: 50% auto;
+    width: 30%;
+    margin: 10% auto;
     padding: 10px;
     box-shadow: 2px 2px 5px #00bfff;
     position: relative;
@@ -35,6 +36,19 @@ const Section = styled.section`
   }
   h3 {
     font-weight: 200;
+  }
+  @media only screen and (max-width:600px)  { 
+    .chirpModal {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background: white;
+      width: 60%;
+      margin: 50% auto;
+      padding: 10px;
+      box-shadow: 2px 2px 5px #00bfff;
+      position: relative;
   }
 }
 `;
