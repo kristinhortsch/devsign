a65826a104b1ac764216ae6c520ad315e3f7397a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-15 14:31:49 -0700

    updated chirps service (getting)

--- src/services/chirps.js
+++ src/services/chirps.js
@@ -1,8 +1,14 @@
 export const getChirps = () => {
-  return Promise.resolve([
-    { id: 1234, text: 'life is great', handle: 'user1', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-    { id: 1235, text: 'life is meh', handle: 'user2', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-    { id: 1236, text: 'life is okay', handle: 'user3', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-    { id: 1237, text: 'life is fabulous', handle: 'user4', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' }
-  ]);
+  return fetch(process.env.API_URL)
+    .then(res=> [res.ok, res.json()])
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+  // Promise.resolve([
+  //   { id: 1234, text: 'life is great', handle: 'user1', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+  //   { id: 1235, text: 'life is meh', handle: 'user2', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+  //   { id: 1236, text: 'life is okay', handle: 'user3', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+  //   { id: 1237, text: 'life is fabulous', handle: 'user4', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' }
+  // ]);
 };
