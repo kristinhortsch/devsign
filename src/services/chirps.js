ed43f97a0ea765e16ee699133a23d124a5ec7f24
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-15 15:33:33 -0700

    added backend route stuff

--- src/services/chirps.js
+++ src/services/chirps.js
@@ -1,14 +1,4 @@
-export const getChirps = () => {
-  return fetch(process.env.API_URL)
-    .then(res=> [res.ok, res.json()])
-    .then(([ok, json]) => {
-      if(!ok) throw json;
-      return json;
-    });
-  // Promise.resolve([
-  //   { id: 1234, text: 'life is great', handle: 'user1', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-  //   { id: 1235, text: 'life is meh', handle: 'user2', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-  //   { id: 1236, text: 'life is okay', handle: 'user3', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
-  //   { id: 1237, text: 'life is fabulous', handle: 'user4', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' }
-  // ]);
-};
+import { get, post } from './request';
+
+export const getChirps = () => get('/chirps');
+export const createChirp = chirp => post('/chirps', chirp);
