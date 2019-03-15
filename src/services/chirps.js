c7a302f5546e6a8748e94e863ad8cf9c72097b3c
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-15 11:32:36 -0700

    fixed seed data

--- src/services/chirps.js
+++ src/services/chirps.js
@@ -1,8 +1,8 @@
 export const getChirps = () => {
   return Promise.resolve([
-    { id: 1234, chirp: 'life is great', handle: 'user1', profileImg: '../../../public/assets/prof-pic.png' },
-    { id: 1235, chirp: 'life is meh', handle: 'user2', profileImg: '../../../public/assets/prof-pic.png' },
-    { id: 1236, chirp: 'life is okay', handle: 'user3', profileImg: '../../../public/assets/prof-pic.png' },
-    { id: 1237, chirp: 'life is fabulous', handle: 'user4', profileImg: '../../../public/assets/prof-pic.png' }
+    { id: 1234, chirp: 'life is great', handle: 'user1', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+    { id: 1235, chirp: 'life is meh', handle: 'user2', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+    { id: 1236, chirp: 'life is okay', handle: 'user3', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' },
+    { id: 1237, chirp: 'life is fabulous', handle: 'user4', profileImg: 'https://res.cloudinary.com/khortsch/image/upload/v1552673467/prof-pic.png' }
   ]);
 };
